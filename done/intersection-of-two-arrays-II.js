dbb257d5dee514694716785c64cef3802ed027af
peacebonez <kmpariso12@gmail.com>
2020-12-17 18:58:41 -0500

    another one

--- done/intersection-of-two-arrays-II.js
+++ done/intersection-of-two-arrays-II.js
@@ -1,37 +1,22 @@
 const intersect = function (nums1, nums2) {
-  const matchArr = [];
+  function looper(arr1, arr2) {
+    const matchArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+      const matchIndex = arr2.indexOf(arr1[i]);
+      if (matchIndex > -1) {
+        matchArr.push(arr1[i]);
+        arr2.splice(matchIndex, 1);
+      }
+    }
+    return matchArr;
+  }
   if (nums1.length !== nums2.length) {
     const longerArr = nums1.length > nums2.length ? nums1 : nums2;
     const shorterArr = nums1.length < nums2.length ? nums1 : nums2;
-
-    function looper(arr1, arr2) {
-      for (let i = 0; i < arr1.length; i++) {
-        const matchIndex = arr2.indexOf(arr1[i]);
-        if (matchIndex > -1) {
-          matchArr.push(shorterArr[i]);
-          longerArr.splice(matchIndex, 1);
-        }
-      }
-    }
-
-    for (let i = 0; i < shorterArr.length; i++) {
-      const matchIndex = longerArr.indexOf(shorterArr[i]);
-      if (matchIndex > -1) {
-        matchArr.push(shorterArr[i]);
-        longerArr.splice(matchIndex, 1);
-      }
-    }
+    return looper(shorterArr, longerArr);
   } else {
-    for (let i = 0; i < nums1.length; i++) {
-      const matchIndex = nums2.indexOf(nums1[i]);
-      if (matchIndex > -1) {
-        matchArr.push(nums1[i]);
-        nums2.splice(matchIndex, 1);
-      }
-    }
+    return looper(nums1, nums2);
   }
-
-  return matchArr;
 };
 
 console.log(intersect([1, 2], [1, 1]));
