e6989af3ca609b294e089d7bd131456ae36d3b1d
peacebonez <kmpariso12@gmail.com>
2020-12-29 19:55:48 -0600

    container with most water done

--- container-with-the-most-water.js
+++ container-with-the-most-water.js
@@ -1,15 +1,23 @@
 const maxArea = (height) => {
-  let curArea = 0,
+  let left = 0,
+    right = height.length - 1,
     max = 0;
 
-  for (let i = 0; i < height.length - 1; i++) {
-    for (let j = i + 1; j < height.length; j++) {}
+  while (left < right) {
+    let lower = Math.min(height[left], height[right]);
+    let curArea = lower * Math.abs(right - left);
+    if (curArea > max) max = curArea;
+    if (height[left] < height[right]) {
+      left++;
+    } else right--;
   }
+  return max;
 };
 
+// console.log(maxArea([1, 2, 4, 3]));
 // console.log(maxArea([1, 2, 1]));
 // console.log(maxArea([4, 3, 2, 1, 4]));
-console.log(maxArea([1, 7, 6, 2, 5, 4, 8, 3, 8]));
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
 // console.log(maxArea([1, 1]));
 
 //multiply lower wall value by difference in indexes between 2 walls
@@ -20,32 +28,66 @@ console.log(maxArea([1, 7, 6, 2, 5, 4, 8, 3, 8]));
 // currentArea = current value * forward index
 
 // const maxArea = (height) => {
-//     let max = 0,
-//       leftWall = null;
+//   let curArea = 0,
+//     max = 0,
+//     lowerWall,
+//     spread,
+//     startWall,
+//     endWall;
+
+//   for (
+//     let start = 0, end = height.length - 1;
+//     start < height.length / 2;
+//     start++, end--
+//   ) {
+//     console.log("height[start]:", height[start]);
+//     console.log("height[end]:", height[end]);
+//     if (height.length === 2) {
+//       return Math.min(height[start], height[end]) * 1;
+//     }
+//     if (start + 1 === end - 1) {
+//       return Math.min(height[start], height[end]) * Math.abs(start - end);
+//     }
+//     if (height[start + 1] < height[start]) startWall = height[start];
+//     if (height[end - 1] < height[end]) endWall = height[end];
+
+//     if (startWall && endWall) {
+//       console.log("startWall:", startWall);
+//       console.log("endWall:", endWall);
+//       lowerWall = Math.min(startWall, endWall);
+
+//       spread = Math.abs(
+//         height.indexOf(startWall) - height.lastIndexOf(endWall)
+//       );
+
+//       curArea = lowerWall * spread;
+
+//       if (curArea > max) max = curArea;
+//       return max;
+//     }
+//     console.log(height);
+//   }
+//   return max;
+// };
+// const maxArea = (height) => {
+//     let curArea = 0,
+//       max = 0,
+//       lowerWall,
+//       spread;
 
 //     for (let i = 0; i < height.length; i++) {
-//       // console.log("leftWall:", leftWall);
-//       console.log("height[i]:", height[i]);
-//       let currentArea = 0;
-//       let lowerWall = Math.min(leftWall, height[i]);
-//       let tallerWall = Math.max(leftWall, height[i]);
-//       // console.log("tallerWall:", tallerWall);
+//       for (let j = i + 1; j < height.length; j++) {
+//         //   console.log("j:", j);
+//         //   console.log("height[j]:", height[j]);
+//         lowerWall = Math.min(height[i], height[j]);
+//         //   let higherWall = Math.max(height[i], height[j])
+//         spread = Math.abs(i - j);
 
-//       //find the left wall
-//       if (i < height.length / 2 && height[i] > leftWall) leftWall = height[i];
-
-//       //find the right wall
-//       if (i >= height.length / 2) {
-//         let spread = Math.abs(
-//           height.indexOf(lowerWall) - height.indexOf(tallerWall)
-//         );
-//         //   console.log("height.indexOf(tallerWall):", height.indexOf(tallerWall));
-//         currentArea = lowerWall * spread;
-//         if (currentArea > max) max = currentArea;
+//         //   console.log("spread:", spread);
+//         curArea = lowerWall * spread;
+//         //   console.log("curArea:", curArea);
+//         if (curArea > max) max = curArea;
 //       }
-//       // console.log("currentArea:", currentArea);
 //     }
-
-//     console.log("max:", max);
 //     return max;
 //   };
