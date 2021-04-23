09f00d8faec0073522cf1d5fc3d6027bb65827d8
peacebonez <kmpariso12@gmail.com>
2021-04-23 11:39:17 -0400

    ANOTHER ONE

---
+++ done/fizzbuzz.js
@@ -0,0 +1,13 @@
+function fizzBuzz(n) {
+  const output = []
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) output.push("FizzBuzz")
+    else if (i % 3 === 0) output.push("Fizz")
+    else if (i % 5 === 0) output.push("Buzz")
+    else output.push(i.toString())
+  }
+  return output
+}
+
+console.log(fizzBuzz(15))
