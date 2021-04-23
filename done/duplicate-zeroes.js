2907b80a896ec6ec1bc717e3ac81dddf53c2d5a4
peacebonez <kmpariso12@gmail.com>
2021-04-23 11:13:28 -0400

    ANOTHER ONE

--- done/duplicate-zeroes.js
+++ done/duplicate-zeroes.js
@@ -9,20 +9,4 @@ function duplicateZeros(arr) {
   }
 }
 
-// function duplicateZeros(arr) {
-//   const FIXED_LENGTH = arr.length
-//   for (let i = 0; i < arr.length; i++) {
-//     // if encounter a zero, nudge every number back one
-//     // always set arr.length back to FIXED_LENGTH
-//     if (arr[i] === 0) {
-//       for (let j = i + 1; j < arr.length; j++) {
-//         arr[j + 1] = arr[j]
-//         console.log(arr)
-//         arr.length = FIXED_LENGTH
-//       }
-//       arr[i + 1] = 0
-//     }
-//   }
-// }
-
 console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]))
